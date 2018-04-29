8fd17d544142d829d4370a4ad1b76b126ac7dfa9
niuyi1017 <1096196462@qq.com>
2018-04-29 15:41:39 +0800

    Tab插件完成

--- Tab选项卡组件/js/tab.js
+++ Tab选项卡组件/js/tab.js
@@ -27,7 +27,24 @@
 		}else if(config.triggerType === "mouseover"||config.triggerType != "click"){
 			this.tabItems.on("mouseover",function(){
 				_this.invoke($(this));
-			})
+			});
+		}
+		
+		
+		if(config.auto){
+			this.timer  = null;
+			this.loop = 0;
+			this.autoPlay();
+			
+			this.tab.hover(function(){
+				window.clearInterval(_this.timer);
+			},function(){
+				_this.autoPlay();
+			});
+		}
+		
+		if(config.invoke>1){
+			this.invoke(this.tabItems.eq(config.invoke-1));
 		}
 		
 		console.log(this.config);
@@ -56,8 +73,44 @@
 				conItems.eq(index).fadeIn().siblings().fadeOut();
 			}
 			
+			if(this.config.auto){
+				this.loop = index;
+			}
+			
+		},
+		autoPlay:function(){
+			var _this = this;
+			var tabItems = this.tabItems;
+			var tabLength = tabItems.length;
+			var config = this.config;
+			
+			this.timer = window.setInterval(function(){
+				_this.loop++;
+				if(_this.loop>=tabLength){
+					_this.loop = 0;
+				}
+				tabItems.eq(_this.loop).trigger(config.triggerType);
+			},config.auto);
 		}
 		
+		
+		
 	};
+	Tab.init = function(tabs){
+			var _this = this;
+			tabs.each(function(){
+				new _this($(this));
+			});
+	}
+	
+	$.fn.extend({
+		tab:function(){
+			this.each(function(){
+				new Tab($(this));
+			});
+			return this;
+		}
+	});
+	
 	window.Tab = Tab; 
 })(jQuery);
