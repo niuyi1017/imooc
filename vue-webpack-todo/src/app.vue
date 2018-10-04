725ee4523e63ce09cadd8bf0e4ec662cc5c52c53
niuyi1017 <1096196462@qq.com>
2018-10-04 14:15:27 +0800

    add vue-webpack-todo

---
+++ vue-webpack-todo/src/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id = "test">{{text}}</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            text:'abc'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #test{
+        color: red;
+    }
+</style>
