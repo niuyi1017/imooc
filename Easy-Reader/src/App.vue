cf8f97f2aa51895dd7032870409d9a81cb253f15
niuyi1017 <1096196462@qq.com>
2018-10-06 18:39:59 +0800

    环境搭建完毕，初始化配置完毕

---
+++ Easy-Reader/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <span class = "icon-back"></span>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
+<style>
+
+</style>
