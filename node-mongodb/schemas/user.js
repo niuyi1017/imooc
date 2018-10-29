657140f29f094150f0e0a5de8169453d927e4e6c
niuyi1017 <1096196462@qq.com>
2018-10-29 22:01:30 +0800

    V 1.3.0

---
+++ node-mongodb/schemas/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const bcrybt = require('bcrypt')
+const SALT_WORK_FACTOR = 10
+const userSchema = new mongoose.Schema({
+  name: {
+    unique: true,
+    type: String
+  },
+  password: Sting,
+  meta: {
+    createAt: {
+      type: Date,
+      default: Date.now()
+    },
+    updateAt: {
+      type: Date,
+      default: Date.now()
+    }
+  }
+})
+// 注意！！！schema中不可用ES6 箭头函数，否则 this = undefined
+userSchema.pre('save', function (next) {
+  let user = this
+  if (this.isNew) {
+    this.meta.createAt = this.meta.updateAt = Date.now()
+  } else {
+    this.meta.updateAt = Date.now()
+  }
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) {
+      return next(err)
+    }
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) {
+        return next(err)
+      }
+      user.password = hash
+      next()
+    })
+  })
+  next()
+})
+
+userSchema.statics = {
+  fetch: function (cb) {
+    return this
+      .find({})
+      .sort('meta.updateAt')
+      .exec(cb)
+  },
+  findById: function (id, cb) {
+    return this
+      .findOne({ _id: id })
+      .exec(cb)
+  }
+}
+module.exports = userSchema
