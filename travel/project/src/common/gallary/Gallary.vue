73a33b04499df0aa1e874bf4f4ea9ddb14289e4a
niuyi1017 <1096196462@qq.com>
2019-01-31 15:29:52 +0800

    banner finish

---
+++ travel/project/src/common/gallary/Gallary.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption" >
+      <swiper-slide v-for="(img, index) in imgs" :key="index" >
+        <img class="gallary-img" :src="img">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallry',
+  data() {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: "fraction",
+        observeParents: true,
+        observer: true
+      },
+    }
+  },
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  },
+}
+</script>
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+  overflow inherit
+.container
+  position fixed
+  top 0
+  bottom 0
+  left 0
+  right 0
+  background #000
+  z-index 99
+  display flex
+  justify-content center
+  flex-direction column
+  .wrapper
+    width 100%
+    height 0
+    padding-bottom 100%
+    .gallary-img{
+      width 100%
+    }
+    .swiper-pagination{
+      color #ffffff
+      bottom -1rem
+    }
+</style>
--- travel/project/src/pages/detail/components/Banner.vue
+++ travel/project/src/pages/detail/components/Banner.vue
@@ -1,15 +1,42 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1705/e9/e993699e058a65a6a3.water.jpg_600x330_58c75a46.jpg">
-    <div class="banner-info">
-      <div class="banner-title">济南世贸宽</div>
-      <div class="banner-number"><span class="iconfont icon-banner">&#xe631;</span>39</div>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1705/e9/e993699e058a65a6a3.water.jpg_600x330_58c75a46.jpg">
+      <div class="banner-info">
+        <div class="banner-title">济南世贸宽</div>
+        <div class="banner-number"><span class="iconfont icon-banner">&#xe631;</span>39</div>
+      </div>
     </div>
+    <gallary 
+          :imgs="imgs" 
+          v-show="showGallary"
+          @close="handleClose"/>
   </div>
+  
 </template>
 <script>
+import Gallary from '@/common/gallary/Gallary'
 export default {
-  name: "DetailBanner"
+  name: "DetailBanner",
+  components: {
+    Gallary
+  },
+  data () {
+    return{
+      imgs: ['//img1.qunarzz.com/sight/p0/1705/e9/e993699e058a65a6a3.water.jpg_600x330_58c75a46.jpg',
+          '//img1.qunarzz.com/sight/p0/1408/20/1d9f6dd0750e74443f69434d616943fe.jpg_600x330_90e2a80c.jpg'
+          ],
+      showGallary: false
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary  = true
+    },
+    handleClose() {
+      this.showGallary  = false
+    }
+  },
 }
 </script>
 <style lang="stylus" scoped>
@@ -28,7 +55,7 @@ export default {
     display flex
     height .6rem
     line-height .6rem
-    background-image linear-gradient(top, rgba(0,0,0,0),rgba(0,0,0,0.8))
+    background-image linear-gradient(to bottom, rgba(0,0,0,0),rgba(0,0,0,0.8))
     .banner-title
       flex 1
       color #fff
