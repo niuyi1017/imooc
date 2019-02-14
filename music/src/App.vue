89151b1dad2a2b3e54ece5c3d6bbf938c7c368b3
niuyi1017 <1096196462@qq.com>
2019-02-14 10:39:38 +0800

    init

--- music/src/App.vue
+++ music/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <m-header></m-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import MHeader from './components/m-header/m-header'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MHeader
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped>
+@import "~@/common/stylus/variable";
+
 </style>
+
+
--- music/src/main.js
+++ music/src/main.js
@@ -1,6 +1,9 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
-
+import fastclick from 'fastclick'
+import '@/common/stylus/index.styl'
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
